7d7337e8ac5f2990546f669e8c66340591ba40c3
Ulisses Gomes <ulissesdeveloper82@gmail.com>
2022-11-01 12:34:05 -0300

    Criado projeto

---
+++ src/test/java/com/api/school/SchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
